54f8e0c39c227dfd5e5ddb99b3299623cc221952
Daniel Lopez <daniel.lopez.pajares.2021@gmail.com>
2022-10-22 00:15:30 +0200

    Modified: name==main

--- users/password_hash.py
+++ users/password_hash.py
@@ -42,9 +42,4 @@ def hash_password(password=None, password_gen=False):
     return hashed
 
 if __name__ == '__main__':
-    password = input('Enter password or type True: ')
-    
-    if password in ['True','true']:
-        print(hash_password(password_gen=True))
-    else:
-        print(hash_password(password))
+    hash_password(password_gen=True)
